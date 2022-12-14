e8881a34fb844c16c38e6e429ae64583c7883f53
Oren Nachman <nachmano@amazon.com>
2022-12-13 22:05:46 -0800

    initial commit

---
+++ OutlookCalendarStaleCleaner/OutlookCalendarStaleCleaner/OutlookHelper.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookCalendarStaleCleaner
+{
+  internal static class OutlookHelper
+  {
+
+    private const int DEFAULT_SEARCH_HOURS = 1;
+    private const int OUTLOOK_LAUNCH_SLEEP_INTERVAL = 15000;
+
+    public static async Task<List<Outlook.Folder>> GetInboxes(bool autoLaunchOutlook)
+    {
+      var folders = new List<Outlook.Folder>();
+
+      if (!autoLaunchOutlook && !OutlookRunning())
+      {
+        return folders;
+      }
+
+      Outlook.Application outlook;
+      Outlook.Stores stores;
+
+      try
+      {
+        await EnsureOutlookIsRunningAsync();
+        
+        outlook = new Outlook.Application();
+        stores = outlook.Session.Stores;
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine($"Exception initializing Outlook in GetInboxes.\n{e}");
+
+        // this can generally be ignored (it's often a COM RETRYLATER when Outlook is stuck
+        // booting up etc). Regardless, there is no remediation, so let's bail.
+        //
+        // Note: Yes, this is a double return, yes the for loop will just exit and it will return
+        //       anyway, but who knows what will get added post the for at some point in the future
+        //       and what chaose that will cause. This exception is exceedingly rare so would rather
+        //       fail fast.
+        return folders;
+      }
+
+      // foreach on COM objects can sometimes get into weird states when encountering
+      // a corrupt pst,  where null objects repeat themselves, and a foreach goes into
+      // an infinite loop, so prefer traditional for
+      //
+      // Note: These are one-based arrays
+      //       See: https://docs.microsoft.com/en-us/dotnet/api/microsoft.office.interop.outlook._stores.item?view=outlook-pia#Microsoft_Office_Interop_Outlook__Stores_Item_System_Object_
+      for (int i = 1; i <= stores?.Count; i++)
+        {
+          Outlook.Store store = null;
+
+          try
+          {
+            // this is in the try since sometimes COM will freak out and throw
+            // IndexOutOfRangeException even though we're < Count (corrupt pst situation)
+            store = stores[i];
+
+            // ignore public folders (causes slow Exchange calls, and we don't have a use case
+            // for interactions with those)
+            if (store.ExchangeStoreType == Outlook.OlExchangeStoreType.olExchangePublicFolder)
+              continue;
+
+            var folder = (Outlook.Folder)store.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            System.Diagnostics.Debug.WriteLine($"Found inbox: {folder.Name} in store {store.DisplayName}");
+
+            folders.Add(folder);
+          }
+          catch (Exception e)
+          {
+            // Not every root folder has an Inbox(for example, Public folders), so this exception can be ignored
+            // This also catches cases where store cannot be retrieved from a specific index (even though the API
+            // says it is there. In those cases you'll see something like:
+            //  System.Runtime.InteropServices.COMException (0x80040119): Outlook cannot start because a data file to
+            //  send and receive messages cannot be found. Check your settings in this Microsoft Outlook profile. <snip>
+            Debug.WriteLine($"Failed to get Inbo for {store?.DisplayName} type: {store?.ExchangeStoreType}:\n{e}");
+          }
+        }
+
+      return folders;
+    }
+
+    internal static bool OutlookRunning()
+    {
+      var outlookProcs = Process.GetProcessesByName("outlook");
+
+      return outlookProcs.Length > 0;
+    }
+
+    private static async Task EnsureOutlookIsRunningAsync()
+    {
+      if (!OutlookRunning())
+      {
+        var startInfo = new ProcessStartInfo("outlook.exe");
+        startInfo.UseShellExecute = true;
+
+        Process.Start(startInfo);
+
+        await Task.Delay(OUTLOOK_LAUNCH_SLEEP_INTERVAL);
+
+        if (!OutlookRunning())
+        {
+          throw new TimeoutException("Timed out waiting for Outlook to launch");
+        }
+      }
+    }
+
+
+
+  }
+}
---
+++ OutlookCalendarStaleCleaner/OutlookCalendarStaleCleaner/Program.cs
@@ -0,0 +1,157 @@
+﻿using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookCalendarStaleCleaner
+{
+
+  static class Program
+  {
+
+    private static class Stats
+    {
+      public static int Deleted = 0;
+      public static int MarkedTenative = 0;
+      public static int Ignored = 0;
+    }
+
+    private static void Usage()
+    {
+      Console.WriteLine(@"
+Simple program to clean stale calendar invites sitting in your inbox. This tool will:
+
+  1. Delete unanswered calendar invites if the meeting happened over 24 hours ago
+     -> Note: meeting will remain on your calendar, just marked as tentative
+  2. Delete calendar invites that have already been accepted (perhaps manually on your calendar)
+  3. Automatically process meeting cancellations
+      ");
+    }
+
+    public static async Task Main(string[] args)
+    {
+      if (args.Length > 0) 
+      {
+        Usage();
+        return;
+      }
+
+      var folders = await OutlookHelper.GetInboxes(false);
+
+      foreach (var folder in folders)
+      {
+        Console.WriteLine($"🔃 Processing Inbox Folder: {folder.FolderPath}");
+
+        CleanStaleItems(folder);
+
+        Console.WriteLine($"✅ Finished Processing Inbox Folder: {folder.FolderPath}\n");
+      }
+
+      Console.WriteLine("\n---------------\n");
+      Console.WriteLine("🏁 Completed!");
+      Console.WriteLine($"  🙈 Ignored         : {Stats.Ignored}");
+      Console.WriteLine($"  ⛺ Marked Tentative: {Stats.MarkedTenative}");
+      Console.WriteLine($"  ❌ Deleted         : {Stats.Deleted}");
+    }
+
+    private static void CleanStaleItems(Outlook.Folder folder)
+    {
+      string filter = "([MessageClass] = 'IPM.Schedule.Meeting.Request' OR [MessageClass] = 'IPM.Schedule.Meeting.Canceled')";
+      var items = folder.Items.Restrict(filter);
+
+      foreach (var item in items)
+      {
+        var meeting = (item is Outlook.MeetingItem ? (Outlook.MeetingItem)item : null);
+
+        if (meeting != null)
+        {
+          var appointment = meeting.GetAssociatedAppointment(true);
+
+          // appointment will be null when it has been deleted manually from the calendar but the
+          // mail item is still there
+          var processed = (appointment == null);
+
+          if (processed)
+          {
+            Console.WriteLine($"❌ Processing already deleted meeting");
+            Console.WriteLine($"  ✉️ {meeting.Subject}");
+            Console.WriteLine($"  📧 From: {meeting.SenderEmailAddress}");
+          }
+          else
+          {
+            Console.WriteLine($"✉️ {appointment.Subject}");
+            Console.WriteLine($"  📧 From: {appointment.Organizer}");
+            Console.WriteLine($"  📆 Scheduled: {appointment.Start} -> {appointment.End}");
+            Console.WriteLine($"  🗿 Response Status: {appointment.ResponseStatus}");
+            Console.WriteLine($"  🚩 Meeting Status: {appointment.MeetingStatus}");
+
+            processed = ProcessMeetingCancellation(appointment);
+
+            if (!processed)
+            {
+              processed = ProcessMeetingRequest(appointment);
+            }
+
+            if (!processed)
+            {
+              Stats.Ignored++;
+              Console.WriteLine(" -> 🙈 Ignored");
+              continue;
+            }
+          }
+
+          if (processed)
+          {
+            // delete the original item from the inbox (will remain on the calendar)
+            meeting.Delete();
+
+            Stats.Deleted++;
+
+            Console.WriteLine(" -> ✅ Cleaned!");
+          }
+        }
+      }
+    }
+
+    private static bool ProcessMeetingRequest(Outlook.AppointmentItem appointment)
+    {
+      // don't remove anything within the last day
+      if (appointment.StartUTC < DateTime.UtcNow.Subtract(TimeSpan.FromDays(1)))
+      {
+        // don't set to tentative if we already accepted (or organized) the meeting,
+        // for example directly via our calendar, or if the meeting is cancelled
+        if (appointment.ResponseStatus != Outlook.OlResponseStatus.olResponseAccepted &&
+            appointment.ResponseStatus != Outlook.OlResponseStatus.olResponseOrganized)
+        {
+          // record a Tentative response
+          var response = appointment.Respond(Outlook.OlMeetingResponse.olMeetingTentative, true, Type.Missing);
+
+          // randomly some responses will come back null
+          if (response != null)
+          {
+            // delete the MeetingItem that is generated as a response so that no repsonse
+            // is sent to the organizer
+            response.Close(Outlook.OlInspectorClose.olDiscard);
+
+            Stats.MarkedTenative++;
+          }
+        }
+
+        return true;
+      }
+
+      return false;
+    }
+
+    private static bool ProcessMeetingCancellation(Outlook.AppointmentItem appointment)
+    {
+      if (appointment.MeetingStatus == Outlook.OlMeetingStatus.olMeetingCanceled ||
+          appointment.MeetingStatus == Outlook.OlMeetingStatus.olMeetingReceivedAndCanceled)
+      {
+        // process all cancellations, regardless of time
+        appointment.Delete();
+
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
